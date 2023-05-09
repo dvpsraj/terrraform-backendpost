cf4975d38aafc484504b197de77397c5f2d7e06b
Raj <dvpsraj@gmail.com>
2023-05-09 06:15:49 +0300

    mycodee

--- remotebackend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "NetworkWatcherRG"
-    storage_account_name = "backendstorage94"
-    container_name       = "tfstate"
-    key                  = "default.terraform.tfstate"
-  }
-}
